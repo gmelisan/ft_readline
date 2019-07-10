3e51ac91f19a73d62102786e98a5da089b37d0f7
gmelisan <gmelisan@student.21-school.ru>
2019-07-11 02:59:14 +0300

    fix resizing

--- display.c
+++ display.c
@@ -6,7 +6,7 @@
 /*   By: gmelisan </var/spool/mail/vladimir>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 11:13:31 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/07/10 23:13:20 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/07/11 02:55:40 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 #include "terminal.h"
 
 t_buffer		g_buffer;
+
+/*
+** In fact we don't need to go to start. May be fix later.
+*/
 
 static void		move_cur_start(void)
 {
@@ -31,12 +35,12 @@ static void		move_cur_start(void)
 		term_putstr("le");
 }
 
-static  void		move_cur_right(int oldpos)
+static  void		move_cur_right(int oldpos, int width)
 {
 	int	oldccol;
 
-	oldccol = oldpos % g_buffer.out.cols;
-	if (oldccol == g_buffer.out.cols - 1)
+	oldccol = oldpos % width;
+	if (oldccol == width - 1)
 	{
 		term_putstr("do");
 		term_putstr("cr");
@@ -45,24 +49,36 @@ static  void		move_cur_right(int oldpos)
 		term_putstr("nd");
 }
 
-static void		move_cur_left(int oldpos)
+static void		move_cur_left(int oldpos, int width)
 {
 	int	oldccol;
 	int	i;
 
-	oldccol = oldpos % g_buffer.out.cols;
+	oldccol = oldpos % width;
 	if (oldccol == 0)
 	{
 		term_putstr("up");
 		i = -1;
-		while (++i < g_buffer.out.cols - 1)
+		while (++i < width - 1)
 			term_putstr("nd");
 	}
 	else
 		term_putstr("le");
 }
 
+/* Not works. Now just using termcap("ce") instead. */
 
+/* static void		clear_to_end(int pos, int width) */
+/* { */
+/* 	int i; */
+
+/* 	i = pos % width - 1; */
+/* 	while (++i < width) */
+/* 		ft_putchar(' '); */
+/* 	i--; */
+/* 	while (i > pos) */
+/* 		move_cur_left(i--, width); */
+/* } */
 
 static t_string	*build_bufout(t_string str, int width)
 {
@@ -85,12 +101,64 @@ static t_string	*build_bufout(t_string str, int width)
 	return (res);
 }
 
-static int		max(int a, int b)
+/*
+** These checks are to be sure we are not out of buffer array when resizing.
+*/
+
+static char		getchar(t_buffer newbuf, int i, int j)
 {
-	return (a > b ? a : b);
+	if (newbuf.out.cols >= g_buffer.out.cols) /* screeen enlarge */
+	{
+		if (i < newbuf.out.rows && j < (int)newbuf.out.b[i].len)
+			return (str_get(newbuf.out.b[i], j));
+		else
+			return (0);
+	}
+	else /* screen shrink */
+	{
+		if (i < g_buffer.out.rows && j < (int)g_buffer.out.b[i].len)
+			return (str_get(g_buffer.out.b[i], j));
+		else
+			return (0);
+	}
 }
 
-static  void	redisplay(t_buffer newbuf)
+static t_string *get_outstr(t_buffer *buf, int i)
+{
+	if (i < buf->out.rows && i >= 0)
+		return (&(buf->out.b[i]));
+	return (NULL);
+}
+
+/*
+** Count how many times we need to step in inner loop of redisplay.
+** We need this checks when resizing.
+** Also we get maximum value of old and new buf's lens because
+** when our new buffer is lesser then old, we need to clear
+** remainings from old.
+*/
+
+static int		cols(t_buffer *newbuf, int i)
+{
+	t_string	*old;
+	t_string	*new;
+	int			newlen;
+	int			oldlen;
+
+	old = get_outstr(newbuf, i);
+	new = get_outstr(&g_buffer, i);
+	oldlen = old ? old->len : 0;
+	newlen = new ? new->len : 0;
+	return (oldlen > newlen ? oldlen : newlen);
+}
+
+/*
+** Main redisplay loop. Now it just redraw all characters, even 
+** if they are the same.
+** TODO: skip repeated characters.
+*/
+
+static  void	redisplay(t_buffer newbuf, int resize)
 {
 	int i;
 	int j;
@@ -99,22 +167,24 @@ static  void	redisplay(t_buffer newbuf)
 	move_cur_start();
 	i = -1;
 	pos = 0;
-	while (++i < newbuf.out.rows)
+	while (++i < max(newbuf.out.rows, g_buffer.out.rows))
 	{
 		j = -1;
-		while (++j < max(newbuf.out.b[i].len, g_buffer.out.b[i].len))
+		while (++j < cols(&newbuf, i))
 		{
-			if (str_get(newbuf.out.b[i], j))
-				ft_putchar(str_get(newbuf.out.b[i], j));
+			if (getchar(newbuf, i, j))
+				ft_putchar(getchar(newbuf, i, j));
 			else
 				ft_putchar(' ');
 			if (pos % newbuf.out.cols == newbuf.out.cols - 1)
-				move_cur_right(pos);
+				move_cur_right(pos, newbuf.out.cols);
 			pos++;
 		}
 	}
+	if (resize)
+		term_putstr("ce");
 	while (pos != newbuf.cpos)
-		move_cur_left(pos--);	
+		move_cur_left(pos--, newbuf.out.cols);
 }
 
 void	init_linebuf(t_line line)
@@ -136,6 +206,15 @@ void	clear_linebuf(void)
 	ft_bzero(&g_buffer, sizeof(t_buffer));
 }
 
+/*
+** Idea as in the original Readline lib.
+** Two buffers: 
+** - `g_buffer' is what on the screen now.
+** - `newbuf' builded from `line', what we want our screen to be.
+** We update screen using `newbuf' and in the end put its contents
+** to `g_buffer'.
+*/
+
 void	update_line(t_line *line)
 {
 	t_buffer			newbuf;
@@ -147,7 +226,7 @@ void	update_line(t_line *line)
 	newbuf.out.rows = newbuf.b.len / ws.ws_col + 1;
 	newbuf.out.cols = ws.ws_col;
 	newbuf.out.b = build_bufout(newbuf.b, ws.ws_col);
-	redisplay(newbuf);
+	redisplay(newbuf, line ? 0 : 1);
 	clear_linebuf();
 	ft_memcpy(&g_buffer, &newbuf, sizeof(t_buffer));
 }
--- signal_handlers.c
+++ signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: gmelisan </var/spool/mail/vladimir>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 21:57:39 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/07/08 17:05:39 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/07/11 00:06:13 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,37 @@ void	sigh_sigint(int sig)
 	}
 }
 
+void	output_buffer(int fd)
+{
+	int i;
+	int j;
+
+	ft_fdprintf(fd, "pos = %d\n", g_buffer.cpos);
+	for (i = 0; i < g_buffer.out.rows; i++)
+	{
+		for (j = 0; j < (int)g_buffer.out.b[i].len; j++)
+			ft_fdprintf(fd, "%c", str_get(g_buffer.out.b[i], j));
+		ft_fdprintf(fd, "\n");
+	}
+}
+
 void	sigh_sigwinch(int sig)
 {
+	struct winsize	ws;
+	int fd_bufout;
+
 	if (sig == SIGWINCH)
+	{
+		ioctl(STDOUT, TIOCGWINSZ, &ws);
+		fd_bufout = open("./bufout", O_RDWR | O_TRUNC | O_CREAT, S_IRWXU);
+		ft_fdprintf(g_logfd, "[SIGWINCH] -> %dx%d\n", ws.ws_col, ws.ws_row);
+		ft_fdprintf(fd_bufout, "before:\n");
+		output_buffer(fd_bufout);
+		ft_fdprintf(fd_bufout, "\n");
 		update_line(NULL);
+		
+		ft_fdprintf(fd_bufout, "after:\n");
+		output_buffer(fd_bufout);
+		close(fd_bufout);
+	}
 }
